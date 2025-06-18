d937a2cc049dab9daf66036c127fae7f22d466e7
Angel Oluwaseun Balogun <angel123@meta.com>
2025-06-18 16:51:25 -0700

    Feature(kudosBoard):Did some basic css styling and put some place holders in the image src , I have also created the board Page and home page)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-
+import BoardPage from "./Pages/BoardPage";
+import './App.css';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/board/:id" element={<BoardPage />} /> */}
-        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="/board/:id" element={<BoardPage />} /> 
+        {/* <Route path="*" element={<NotFound />} />  */}
       </Routes>
     </Router>
   );
